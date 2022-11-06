c337649baccb41b032d10266bd5599021421cc4b
clarsongit <clarson@berkeley.edu>
2022-11-06 15:02:20 -0800

    Updated the time

--- src/script.js
+++ src/script.js
@@ -1,45 +1,44 @@
-//// Display Day & Time of Current Location
-let now = new Date();
-let day = now.getDay();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-day = days[now.getDay()];
-
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-let dayTime = `${day} ${hours}:${minutes}`;
-
-let displayDay = document.querySelector("h2");
-displayDay.innerHTML = dayTime;
-
 //// API Integration
 function formatDate(timestamp) {
   let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  day = days[date.getDay()];
+  let formattedDate = `${day} ${hours}:${minutes}`;
+  return formattedDate;
 }
 
 function displayTemp(response) {
   let currentTemp = Math.round(response.data.main.temp);
   let tempElement = document.querySelector("#temp");
   tempElement.innerHTML = `${currentTemp}°`;
+  let city = response.data.name;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = city;
+  let description = response.data.weather[0].description;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = description;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-let city = "San Diego";
+let city = "Paris";
 let apiKey = "4f1c03a70540f345ecd651bb7c938490";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&&appid=${apiKey}&units=metric`;
 
