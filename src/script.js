458bd1224b869137f8ff19c8ecb4624e33e79b24
clarsongit <clarson@berkeley.edu>
2022-11-10 15:30:04 -0800

    Updated weather icons

--- src/script.js
+++ src/script.js
@@ -27,7 +27,7 @@ function formatDate(timestamp) {
 function displayTemp(response) {
   let currentTemp = Math.round(response.data.main.temp);
   let tempElement = document.querySelector("#temp");
-  tempElement.innerHTML = `${currentTemp}°`;
+  tempElement.innerHTML = `${currentTemp}`;
   let city = response.data.name;
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = city;
@@ -36,9 +36,16 @@ function displayTemp(response) {
   descriptionElement.innerHTML = description;
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#weather-icon");
+
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let city = "Paris";
+let city = "San Diego";
 let apiKey = "4f1c03a70540f345ecd651bb7c938490";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&&appid=${apiKey}&units=metric`;
 
