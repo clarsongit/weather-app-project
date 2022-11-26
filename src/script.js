71ecfe0bcffbc5a2459dc4fbe1901c519aaf0698
clarsongit <clarson@berkeley.edu>
2022-11-26 13:54:38 -0800

    Injected forecast HTML template via JS

--- src/script.js
+++ src/script.js
@@ -24,8 +24,32 @@ function formatDate(timestamp) {
   return formattedDate;
 }
 
-function displayForecast(response) {
-  console.log(response.data.daily);
+function displayForecast() {
+  //console.log(response.data.daily);
+
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class = "row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://openweathermap.org/img/wn/50d@2x.png"
+                alt=""
+                width="42"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max"> 18° </span>
+                <span class="weather-forecast-temperature-min"> 12° </span>
+              </div>
+            </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
@@ -62,7 +86,7 @@ function displayTemp(response) {
   windSpeed = response.data.wind.speed;
   windElement.innerHTML = `Wind speed: ${windSpeed}km/hr`;
 
-  getForecast(response.data.coord);
+  //  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -107,3 +131,6 @@ fahrenheitLink.addEventListener("click", displayFahrenheit);
 
 let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelcius);
+
+search("Paris");
+displayForecast();
